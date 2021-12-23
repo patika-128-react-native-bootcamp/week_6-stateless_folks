2737dc3e7dc6cbe69f2a702ee8279307e69e5603
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-24 01:10:38 +0300

    Settings page created

---
+++ src/pages/Settings/Setings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+export default function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+}
