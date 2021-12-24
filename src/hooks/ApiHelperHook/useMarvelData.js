357ca1e243cf362c2e934e67f70805ae20a453d0
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-24 15:53:38 +0300

    removed onsole.log

--- src/hooks/ApiHelperHook/useMarvelData.js
+++ src/hooks/ApiHelperHook/useMarvelData.js
@@ -6,7 +6,6 @@ function useMarvelData(parameter) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
   const publicKey = Config.PUBLIC_KEY;
   const hash = Config.HASH;
   const ts = Config.T; // timestamp default maybe change later
@@ -18,14 +17,10 @@ function useMarvelData(parameter) {
     setIsLoading(true);
     async function fetchMarvel() {
       try {
-        console.log('response');
         const fetchData = `${url}/${parameter}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-        console.log(fetchData);
         const response = await axios.get(fetchData);
         //const res = await response.json();
-        console.log('response1');
         setError(null);
-
         setData(response.data);
         setIsLoading(false);
       } catch (err) {
