5bebb2ed72798933622920c2ce7f918261e0681a
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-24 23:35:13 +0300

    BottomTab Navigation Setup

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../pages/HomeScreen';
+import SearchScreen from '../pages/SearchScreen';
+import SettingsScreen from '../pages/SettingsScreen';
+import BookmarkScreen from '../pages/BookmarkScreen';
+import routes from './routes';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      {
+        /* Rest of your app code */
+        <Tab.Navigator>
+          <Tab.Screen name={routes.HOME_SCREEN} component={HomeScreen} />
+          <Tab.Screen
+            name={routes.SETTINGS_SCREEN}
+            component={SettingsScreen}
+          />
+          <Tab.Screen name={routes.SEARCH_SCREEN} component={SearchScreen} />
+          <Tab.Screen
+            name={routes.BOOKMARK_SCREEN}
+            component={BookmarkScreen}
+          />
+        </Tab.Navigator>
+      }
+    </NavigationContainer>
   );
 }
--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,1 +1,8 @@
-export default {};
+export default {
+  HOME_SCREEN: 'HomeScreen',
+  SEE_MORE_SCREEN: 'SeeMoreScreen',
+  DETAIL_SCREEN: 'DetailScreen',
+  SETTINGS_SCREEN: 'SettingsScreen',
+  BOOKMARK_SCREEN: 'BookmarkScreen',
+  SEARCH_SCREEN: 'SearchScreen',
+};
