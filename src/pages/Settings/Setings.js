a8551629f147b60e67f94a27dd53eca4478afeda
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-24 14:55:06 +0300

    Settings page created.

--- src/pages/Settings/Setings.js
+++ src/pages/Settings/Setings.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, Button} from 'react-native';
+import ThemeProvider, {
+  ThemeContext,
+} from '../../context/ThemeContext/ThemeProvider';
 
 export default function Settings() {
+  const {state, dispatch} = useContext(ThemeContext);
+
+  function darkTheme() {
+    dispatch({type: 'dark'});
+  }
+  function lightTheme() {
+    dispatch({type: 'light'});
+  }
   return (
     <View>
-      <Text>Settings</Text>
+      <Button onPress={darkTheme} title="dark" />
+      <Button onPress={lightTheme} title="light" />
+      <Text>{state.theme}</Text>
     </View>
   );
 }
