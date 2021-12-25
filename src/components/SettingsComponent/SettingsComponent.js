58594d15823b316b2fe351cea54da8ef54707d73
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-25 23:59:48 +0300

    Multi Language Support Added

--- src/components/SettingsComponent/SettingsComponent.js
+++ src/components/SettingsComponent/SettingsComponent.js
@@ -5,12 +5,14 @@ import {ThemeContext} from '../../context/ThemeContext/ThemeProvider';
 import styles from './SettingsComponent.styles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import '../../language/i18n';
+import {useTranslation} from 'react-i18next';
 
 export default function SettingsComponent() {
   const {state, dispatch} = useContext(ThemeContext);
   const {colors} = useTheme();
   const [dark, setDark] = useState(false);
-
+  const {t, i18n} = useTranslation();
   async function darkTheme() {
     await AsyncStorage.setItem('theme', 'dark');
     dispatch({type: 'dark'});
@@ -19,11 +21,14 @@ export default function SettingsComponent() {
     await AsyncStorage.setItem('theme', 'light');
     dispatch({type: 'light'});
   }
-  //toggle-switch
+
+  function changeLanguage() {
+    i18n.changeLanguage(i18n.language === 'en' ? 'tr' : 'en');
+  }
   return (
     <View style={styles.container}>
       <View>
-        <Text style={{color: colors.text}}>Change Theme</Text>
+        <Text style={{color: colors.text}}>{t('Change Theme')}</Text>
       </View>
       <View>
         {/* <Text style={{color: colors.text}}>Icon</Text>  */}
@@ -37,6 +42,9 @@ export default function SettingsComponent() {
       </View>
       <Button onPress={darkTheme} title="dark" />
       <Button onPress={lightTheme} title="light" />
+      <Button onPress={changeLanguage} title={t('Change Language')} />
     </View>
   );
 }
+
+//i18next.changeLanguage("en-US-xx");
