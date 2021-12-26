edc9f0639571adf4a3923a88648b16b3bf16254d
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-27 00:16:29 +0300

    feat: :sparkles: Added Comics Card

--- src/components/layouts/ComicsLayout/ComicsLayout.js
+++ src/components/layouts/ComicsLayout/ComicsLayout.js
@@ -1,10 +1,36 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import {View, Text, FlatList, ActivityIndicator} from 'react-native';
+import ComicCard from '../../ComicCard';
+import useMarvelData from '../../../hooks/ApiHelperHook';
 
 export default function ComicsLayout() {
+  const {data, error, isLoading} = useMarvelData('comics');
+  const comics = data;
+  //console.log(comics.data.results.length);
+  function renderItem({item}) {
+    return (
+      item.images[0] &&
+      item.textObjects[0] && (
+        <ComicCard
+          key={item.id}
+          comicName={item.title}
+          comicDetail={item.textObjects[0].text}
+          imageUrl={item.images[0].path + '.' + item.images[0].extension}
+        />
+      )
+    );
+  }
   return (
     <View>
-      <Text>Comics</Text>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={comics.data.results.slice(0, 7)}
+          renderItem={renderItem}
+          horizontal
+        />
+      )}
     </View>
   );
 }
